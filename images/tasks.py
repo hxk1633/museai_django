ea9b94df236aa858ea86bc8161c3be7ca3be725d
harrison <hxk1633@g.rit.edu>
2019-08-05 13:39:03 -0400

    fixing bugs

--- images/tasks.py
+++ images/tasks.py
@@ -15,18 +15,18 @@ def new_pins():
 
 @shared_task
 def new_model(album_id):
+    print("training model " + album_id)
     album = Album.objects.get(id=album_id)
-    #gpu_machine = ["", "", ""]
     system_clean = ["docker", "system", "prune", "-a"]
     build_cmd = ["docker", "build", "-t", "model-builder", "."]
     run_cmd = ["docker", "run", "-v", "/home/django/museai_django/media/albums/" + album.name + "/data:/data", "-it", "model-builder"]
-    Album.objects.get(id=album_id).update(model_status='t')
+    Album.objects.filter(id=album_id).update(model_status='t')
     build = Popen(build_cmd, stdout=PIPE)
     run = Popen(run_cmd, stdout=PIPE)
     print("model created")
     print(build.communicate())
     print(run.communicate())
-    Album.objects.get(id=album_id).update(model_status='c')
+    Album.objects.filter(id=album_id).update(model_status='c')
     tfmodel = TFModel.objects.create(name=album.name, album_model=album)
 
 @shared_task
